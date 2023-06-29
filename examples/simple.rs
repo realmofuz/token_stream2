74f3a83b2156b62a4b357feb042cd27b674d671c
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-06-29 15:58:14 -0400

    t

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,7 @@
 fn main() {
     let to_parse: proc_macro2::TokenStream = r#"
     adwmn3qwbjhd32hgj32hd 23kdh jh23hd jhk2hdjk32 hjshd hjdh32 jkh32d d23ld; al;dasl;d ald"a" "a" "a"
+    1 2 3 4 5.335435 0xff 9.32234423 032324.392394 /234483284 *++++ != != 1-122^^&&%$54%$^
     "#
         .parse()
         .expect("infallible");
